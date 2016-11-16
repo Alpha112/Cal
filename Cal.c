fd59f28184c614e3588af5abe8dcc2f312fcfef9
shuhaibkt <shuhaibkt.me@gmail.com>
2016-11-16 21:36:21 +0530

    cal.c

---
+++ Cal.c
@@ -0,0 +1,11 @@
+void main()
+{
+  int a,b,sum;
+    printf("Enter your 1st number : ");
+    scanf("%d",&a);
+    printf("Enter your 2nd number : ");
+    scanf("%d",&b);
+    sum=a+b;
+    printf(" Your result is :%d",sum);
+    return 0;
+}
